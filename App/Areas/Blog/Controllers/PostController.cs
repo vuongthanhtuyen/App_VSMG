d9b387940728e59fe97109381408d949e0b7b66e
vuongthanhtuyen <112794129+vuongthanhtuyen@users.noreply.github.com>
2024-04-11 21:27:46 +0700

    Done Post: Edit and Create
    Hôm trước đã xong chi tiết và xóa rồi, nay xong thêm phần chỉnh sửa và Tạo mới post

--- App/Areas/Blog/Controllers/PostController.cs
+++ App/Areas/Blog/Controllers/PostController.cs
@@ -13,6 +13,7 @@ using App.Areas.Blog.Models;
 using Microsoft.AspNetCore.Identity;
 using App.Utilities;
 using App.Areas.Blog.Models;
+using System.Collections.Specialized;
 
 namespace AppMvc.Areas.Blog.Controllers
 {
@@ -33,13 +34,47 @@ namespace AppMvc.Areas.Blog.Controllers
         [TempData]
         public string StatusMessage { get; set; }
         // GET: Blog/Post
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index([FromQuery(Name ="p")] int currentPage, int pagesize)
         {
+            
+            // Lấy ra các bài post có số lượng ngày cập nhập gần nhất
             var posts = _context.Posts
                 .Include(p => p.Author)
                 .OrderByDescending(p => p.DateUpdated);
+            int totalPost = await posts.CountAsync();
+            if (pagesize < 10) pagesize =10;
+            int countPages = (int)Math.Ceiling((double)totalPost / pagesize);
 
-            return View(posts);
+            if(currentPage> countPages) currentPage = countPages;
+
+            if (currentPage < 1) currentPage = 1;
+
+            var pagingModel = new PagingModel()
+            {
+                countpages = countPages,
+                currentpage = currentPage,
+                generateUrl = (pageNumber) => Url.Action("Index", new
+                {
+                    p = pageNumber,
+                    pagesize = pagesize
+                })
+            };
+
+
+
+            ViewBag.pagingModel = pagingModel;
+            ViewBag.totalPost = totalPost;
+            ViewBag.postIndex = (currentPage - 1) * pagesize;
+
+
+            var postsInPage = await posts.Skip((currentPage - 1) * pagesize)
+                .Take(pagesize)
+                .Include(p => p.PostCategories)
+                .ThenInclude(pc => pc.Category)
+                .ToListAsync();
+
+
+            return View(postsInPage);
         }
 
 
@@ -134,8 +169,8 @@ namespace AppMvc.Areas.Blog.Controllers
             }
 
             StatusMessage = "Bạn đã xóa thành công!!";
-            //_context.Remove(post);
-            // await _context.SaveChangesAsync();
+            _context.Remove(post);
+            await _context.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index));
         }
@@ -180,19 +215,85 @@ namespace AppMvc.Areas.Blog.Controllers
         [HttpPost]
         public async Task<IActionResult> Edit(int id, [Bind("PostId,Title,Content,Description,Slug,Published,CategoryIDs")] CreatePostModel post)
         {
-            if(id !=post.PostId)
+            if (id != post.PostId)
             {
                 return NotFound();
             }
             var categories = await _context.Categories.ToListAsync();
-            ViewData["categories"] = new MultiSelectList(categories,"Id", "Title"); 
+            ViewData["categories"] = new MultiSelectList(categories, "Id", "Title");
 
-            if(post.Slug == null)
+            if (post.Slug == null)
             {
                 post.Slug = AppUtilities.GenerateSlug(post.Title);
             }
-            
+
+            var posts = await _context.Posts.FindAsync(post.PostId);
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var postUpdate = await _context.Posts.Include(p => p.PostCategories)
+                        .FirstOrDefaultAsync(p => p.PostId == id);
+                    if (postUpdate == null) { return NotFound(); }
+                    postUpdate.Title = post.Title;
+                    postUpdate.Slug = post.Slug;
+                    postUpdate.Description = post.Description;
+                    postUpdate.Content = post.Content;
+                    postUpdate.DateUpdated = DateTime.Now;
+                    postUpdate.Published = post.Published;
+
+
+                    // Update PostCategory
+                    if (post.CategoryIDs == null) post.CategoryIDs = new int[] { };
+                    var oldCateIds = postUpdate.PostCategories.Select(c => c.CategoryID).ToArray();
+                    var newCateIds = post.CategoryIDs;
+
+                    var removeCatePosts = from postCate in postUpdate.PostCategories
+                                          where (!newCateIds.Contains(postCate.CategoryID))
+                                          select postCate;
+
+                    _context.PostCategoties.RemoveRange(removeCatePosts);
+                    var addCateIds = from CateId in newCateIds
+                                     where !oldCateIds.Contains(CateId)
+                                     select CateId;
+                    foreach(var cateId in addCateIds)
+                    {
+                        _context.PostCategoties.Add(new PostCategory()
+                        {
+                            PostID = id,
+                            CategoryID = cateId
+                        });
+                    }
+
+
+                    _context.Update(postUpdate);
+                    await _context.SaveChangesAsync();
+                    StatusMessage = "Bạn đã cập nhập thành công";
+
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostExists(post.PostId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+            }
+            //await _context.Update(post);
+
+
+
             return RedirectToAction(nameof(Index));
+        }
+        private bool PostExists(int id)
+        {
+            return _context.Posts.Any(e => e.PostId == id);
         }
 
 
@@ -200,7 +301,7 @@ namespace AppMvc.Areas.Blog.Controllers
 }
 
 
-// Đã làm xong create - nhưng chưa phân trang  - Tối chỉnh tiếp
-// Edit - tối làm
+// Đã làm xong create - done
+// Edit - Done
 // Delete Done
 // Details Done
