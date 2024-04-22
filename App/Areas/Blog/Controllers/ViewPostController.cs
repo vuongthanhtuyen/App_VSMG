0f05fb592e15979e73a693af381fe8eea3e86db3
vuongthanhtuyen <112794129+vuongthanhtuyen@users.noreply.github.com>
2024-04-22 10:27:34 +0700

    add file Blog but not done

---
+++ App/Areas/Blog/Controllers/ViewPostController.cs
@@ -0,0 +1,28 @@
+﻿using App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+
+namespace App.Areas.Blog.Controllers
+{
+    [Area("Blog")]
+    public class ViewPostController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ViewPostController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("/post/{categoryslug?}")]
+        public async Task<IActionResult> Index()
+        {
+
+            var posts = _context.Posts
+                .AsQueryable() // Chuyển đổi dữ liệu từ tập hợp Posts thành một đối tượng IQueryable
+
+                .ToList(); 
+            return View(posts) ;
+        }
+    }
+}
--- App/obj/Debug/net8.0/App.AssemblyInfo.cs
+++ App/obj/Debug/net8.0/App.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3374c3fb028a9ff17bb527a66a09a17a3315bec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b77193887840ee23c67ace54019df640dd40e387")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
